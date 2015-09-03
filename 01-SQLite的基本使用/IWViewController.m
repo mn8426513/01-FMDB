80d64f98dd7a6ccce93345474ef220bed9ea7fdb
毛宁 <919129175@qq.com>
2015-09-04 00:47:57 +0800

    FMDB简单使用

--- 01-SQLite的基本使用/IWViewController.m
+++ 01-SQLite的基本使用/IWViewController.m
@@ -7,9 +7,14 @@
 //
 
 #import "IWViewController.h"
+#import "FMDB.h"
 
 @interface IWViewController ()
-
+@property (nonatomic, strong) FMDatabase *db;
+- (IBAction)insert;
+- (IBAction)update;
+- (IBAction)delete;
+- (IBAction)query;
 @end
 
 @implementation IWViewController
@@ -17,13 +22,61 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+    
+    // 0.获得沙盒中的数据库文件名
+    NSString *filename = [[NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES) lastObject] stringByAppendingPathComponent:@"student.sqlite"];
+    
+    // 1.创建数据库实例对象
+    self.db = [FMDatabase databaseWithPath:filename];
+   
+    // 2.打开数据库
+    if ( [self.db open] ) {
+        NSLog(@"数据库打开成功");
+        
+        // 创表
+        BOOL result = [self.db executeUpdate:@"create table if not exists t_student (id integer primary key autoincrement, name text, age integer);"];
+        
+        if (result) {
+            NSLog(@"创表成功");
+        } else {
+            NSLog(@"创表失败");
+        }
+    } else {
+        NSLog(@"数据库打开失败");
+    }
 }
 
-- (void)didReceiveMemoryWarning
+- (IBAction)insert
 {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+    for (int i = 0; i<40; i++) {
+        NSString *name = [NSString stringWithFormat:@"rose-%d", arc4random() % 1000];
+        NSNumber *age = @(arc4random() % 100 + 1);
+        [self.db executeUpdate:@"insert into t_student (name, age) values (?, ?);", name, age];
+    }
 }
 
+- (IBAction)update
+{
+    [self.db executeUpdate:@"update t_student set age = ? where name = ?;", @20, @"jack"];
+}
+
+- (IBAction)delete
+{
+    
+}
+
+- (IBAction)query
+{
+    // 1.查询数据
+    FMResultSet *rs = [self.db executeQuery:@"select * from t_student where age > ?;", @50];
+    
+    // 2.遍历结果集
+    while (rs.next) {
+        int ID = [rs intForColumn:@"id"];
+        NSString *name = [rs stringForColumn:@"name"];
+        int age = [rs intForColumn:@"age"];
+        
+        NSLog(@"%d %@ %d", ID, name, age);
+    }
+}
 @end
